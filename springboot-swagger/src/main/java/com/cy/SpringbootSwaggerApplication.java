325ad3f3090264819462e61975b20db912ab43bd
86158 <hl809490705@gmail.com>
2020-12-15 23:07:03 +0800

    springboot-swagger init

---
+++ springboot-swagger/src/main/java/com/cy/SpringbootSwaggerApplication.java
@@ -0,0 +1,16 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 86158
+ */
+@SpringBootApplication
+public class SpringbootSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerApplication.class, args);
+    }
+
+}
