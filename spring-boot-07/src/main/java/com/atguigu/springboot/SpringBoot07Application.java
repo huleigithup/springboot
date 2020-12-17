ed2f837ea9d4cdd3a2c42ded5de629fd9df8d454
86158 <hl809490705@gmail.com>
2020-12-17 23:47:07 +0800

    qiyu wendang

---
+++ spring-boot-07/src/main/java/com/atguigu/springboot/SpringBoot07Application.java
@@ -0,0 +1,16 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 86158
+ */
+@SpringBootApplication
+public class SpringBoot07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot07Application.class, args);
+	}
+}
